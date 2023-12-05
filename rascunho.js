c758e2a27489c8b89b976e5ecadb3bc1a4e20250
ev-lira <147561543+ev-lira@users.noreply.github.com>
2023-12-05 20:49:22 -0300

    Update rascunho.js

--- rascunho.js
+++ rascunho.js
@@ -1,57 +1,32 @@
-function () {
-    carregarCadastro();
-  };
-  
-  function cadastrarLogin() {
-    var nome = document.getElementById("Nome Completo").value;
-    var email = document.getElementById("Endereço de Email").value;
-    var senha = document.getElementById("Senha").value;
-  
-    if (nome && email && senha) {
-      var novoLogin = {
-        Nome: nome
-        Endereço de Email: email,
-        Senha: senha,
-      };
-  
-      // Salva a vaga no localStorage
-      salvarLogin(novoLogin);
-  
-      // Adiciona a vaga à lista na interface
-      adicionarCadastroNaLista(novoLogin);
-  
-      // Limpa o formulário após o cadastro
-      document.getElementById("nome").value = "";
-      document.getElementById("email").value = "";
-      document.getElementById("senha").value = "";
-    } else {
-      alert("Preencha todos os campos do formulário.");
-    }
-  }
-  
-  function adicionarCadastroNaLista(Cadastros) {
-    var listaCadastros = document.getElementById("listaCadastros");
-    var novoLogin = document.createElement("li");
-    novoLogin.innerHTML = `<strong>${nome.email.senha}</strong>: ${nome.email.senha}`;
-    listaCadastros.appendChild(novoLogin);
-  }
-  
-  function salvarLogin(Cadastros) {
-    // Recupera as vagas salvas no localStorage
-    var CadastrosSalvos = JSON.parse(localStorage.getItem("Cadastros")) || [];
-  
-    // Adiciona a nova vaga à lista de vagas
-    CadastrosSalvos.push(Cadastros);
-  
-    // Salva a lista atualizada no localStorage
-    localStorage.setItem("Cadastros", JSON.stringify(CadastrosSalvos));
-  }
-  
-  function carregarCadastrosSalvos() {
-    // Recupera as vagas salvas no localStorage
-    var CadastrosSalvos = JSON.parse(localStorage.getItem("Cadastros")) || [];
-  
-    // Adiciona as vagas à lista na interface
-    CadastrosSalvos.forEach(adicionarVagaNaLista(Cadastros));
-  }
-  
+document.addEventListener('DOMContentLoaded', function () {
+  carregarUsuarios();
+
+  document.getElementById('cadastroForm').addEventListener('submit', function (event) {
+      event.preventDefault();
+      cadastrarUsuario();
+  });
+});
+
+function cadastrarUsuario() {
+
+  carregarUsuarios();
+}
+
+function carregarUsuarios() {
+  var usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+
+
+  var listaUsuarios = document.getElementById('listaUsuarios');
+
+
+  listaUsuarios.innerHTML = '';
+
+
+  usuarios.forEach(function (usuario) {
+      var itemLista = document.createElement('li');
+      itemLista.textContent = usuario.nome + ' - ' + usuario.email;
+      listaUsuarios.appendChild(itemLista);
+  });
+}
+
+
